3a792bef20c7320c98bc1030b4b94def807a67a5
tatendabakozw <tatendabakozw@gmail.com>
2021-01-12 03:35:32 +0200

    added front end

---
+++ README.md
@@ -0,0 +1,15 @@
+Project calculates the nearest distance between two different points
+
+It incooperates a geocoder that pulls longitude and latitude from a given address
+
+ALGORITHM USED  
+-- to calculate the nearest distance is used the harvesines formula
+
+    --- a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+        c = 2 ⋅ atan2( √a, √(1−a) )
+        d = R ⋅ c
+
+
+    where  - φ is the latitude
+            - λ is the longitude
+            -R is the radius of the earth = 6.371km
--- server.js
+++ server.js
@@ -1,23 +1,73 @@
 const express = require('express')
 const app = express()
 const PORT = 5500
+const NodeGeocoder = require('node-geocoder');
+const path = require('path');
+const router = express.Router()
 
+//app level middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-//declaring routes
-const locationRoute = require('./routes')
-app.use('/location/api', locationRoute)
+//to render the page
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+
+//the geocoder to give longitude and latitude from  address
+const options = {
+    provider: 'mapquest',
+  
+    // Optional depending on the providers
+    httpAdapter : 'https',
+    apiKey: 'PzwUIZlubKh5osYPD0NqqolWUHUxoUI6', // for Mapquest, OpenCage, Google Premier
+    formatter: null // 'gpx', 'string', ...
+};
+const geocoder = NodeGeocoder(options);
 
 //middleware
-app.use('/',(req,res)=>{
-    res.send('Distance Api')
+app.post('/', async(req,res)=>{
+    try {
+        //address from request bosy
+    const {address, address2} = req.body
+
+    //pulling longitude and latitude from geocoder
+    const addressInfo = await geocoder.geocode(address)
+    const addressInfo2 = await geocoder.geocode(address2)
+
+    if(addressInfo){
+        const lat1 = addressInfo[0].latitude
+        const lon1 = addressInfo[0].longitude
+        const lat2 = addressInfo2[0].latitude
+        const lon2 = addressInfo2[0].longitude
+
+         //the radius of the earth
+        const R = 6378100 //is is metres
+
+        //conveting the longitude and latitude to radians
+        const la1 = lat1* Math.PI/180
+        const la2 = lat2*Math.PI/180
+
+        // //change in latitude in radians
+        const latChange = (lat2-lat1) * Math.PI/180;
+        const lonChange = (lon2-lon1) * Math.PI/180;
+
+        // // calculating a
+        const a = Math.sin(latChange/2) * Math.sin(latChange/2) + Math.cos(la1)*Math.cos(la2)*Math.sin(lonChange/2)*Math.sin(lonChange/2)
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        const distance = R*c
+
+        res.json({distance: distance})
+
+    }else{
+        res.status(403).jsom({error: 'Could not find your address'})
+    }
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
 })
-
-//connect database
-const connectDB = require('./db')
-connectDB();
-
 
 //listener
 app.listen(PORT,(err)=>{
